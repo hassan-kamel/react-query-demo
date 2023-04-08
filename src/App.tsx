55c41fc4a9c38d02b2d254573459781edcc55c52
hassan mohamed kamel <hassankamel755@gmail.com>
2023-04-08 02:38:25 +0200

    ****

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+type post = {
+  id: number | string;
+  title: string;
+};
+const POSTS: post[] = [
+  { id: 1, title: 'post one' },
+  { id: 2, title: 'post two' },
+  { id: 3, title: 'post three' },
+];
 function App() {
-  const [count, setCount] = useState(0);
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
 
-  return <div className='App'></div>;
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) =>
+      wait(1000).then(() =>
+        POSTS.push({ id: String(crypto.randomUUID()), title }),
+      ),
+    onSuccess: () => {
+      console.log('hi');
+      queryClient.invalidateQueries(['posts']);
+    },
+  });
+  const wait = (duration: number) => {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+  };
+
+  if (postQuery.isLoading) return <h1>Loading ----- </h1>;
+  if (postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>;
+
+  return (
+    <div>
+      {postQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button onClick={() => newPostMutation.mutate('new post')}>
+        add new
+      </button>
+    </div>
+  );
 }
 
 export default App;
